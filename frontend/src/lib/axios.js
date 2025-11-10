c1f7f2bbccce665a8da64e1f7541074e9cd5227c
lance <chilance44@gmail.com>
2025-11-11 00:03:30 +0800

    Fix deployment configuration

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? "https://chatyy-qi2e.onrender.com"
-      : "/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "https://chatyy-qi2e.onrender.com/api" 
+    : "/api",
   withCredentials: true,
 });
